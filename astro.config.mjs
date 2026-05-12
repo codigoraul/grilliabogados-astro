094ad4dee63017dbdb2ebd6ef309c89df3c103bb
Raul r <prueba@correo.com>
2026-05-12 01:09:39 -0400

    Fix: Ensure all paths work correctly with /prueba base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://www.grilliabogados.cl/prueba',
-  vite: {
-    build: {
-      assetsDir: '_astro',
-      rollupOptions: {
-        output: {
-          assetFileNames: '_astro/[name].[hash][extname]',
-          chunkFileNames: '_astro/[name].[hash].js',
-          entryFileNames: '_astro/[name].[hash].js'
-        }
-      }
-    },
-    base: './'
+  site: 'https://www.grilliabogados.cl',
+  base: '/prueba',
+  build: {
+    assetsPrefix: '.'
   }
 });
