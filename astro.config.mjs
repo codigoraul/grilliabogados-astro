dd9c843c4d018915aec49b9dce408d122d5bd1aa
Raul r <prueba@correo.com>
2026-05-12 00:55:50 -0400

    Configure vite base for relative paths with dot

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,17 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://www.grilliabogados.cl/prueba',
+  vite: {
+    build: {
+      assetsDir: '_astro',
+      rollupOptions: {
+        output: {
+          assetFileNames: '_astro/[name].[hash][extname]',
+          chunkFileNames: '_astro/[name].[hash].js',
+          entryFileNames: '_astro/[name].[hash].js'
+        }
+      }
+    },
+    base: './'
+  }
 });
